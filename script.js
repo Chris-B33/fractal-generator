2fd469bd168c8a7acd945be6a423ba3dfd03a548
Chris <brophy.c03@gmail.com>
2025-02-03 21:20:35 +0000

    Added a little styling

--- script.js
+++ script.js
@@ -6,28 +6,41 @@
 */
 
 var method_select = document.getElementById("method-select");
-var method_name = ""
+var output_canvas = document.getElementById("output-canvas");
+var method_name = "";
+
+function setOutputCanvasVisibiility(state) {
+    if (state == "on") {
+        output_canvas.classList.remove("hidden-general");
+    } else {
+        output_canvas.classList.add("hidden-general");
+    }
+}
 
 method_select.addEventListener("change", function() {
     method_name = this.options[this.selectedIndex].getAttribute("value");
+    setOutputCanvasVisibiility("off")
 
     for (var target of document.getElementsByClassName("method-select-target")) {
-        if (target.id == method_name + "-method-section") {
-            target.classList.remove("hidden");
+        if (target.id == `${method_name}-method-section`) {
+            target.classList.remove("hidden-method");
         } else {
-            target.classList.add("hidden");
+            target.classList.add("hidden-method");
         }
     }
 })
 
 function generate_additive() {
+    setOutputCanvasVisibiility("on")
     console.log("[INFO]: Generating fractal with method: Additive.")
 }
 
 function generate_reductive() {
+    setOutputCanvasVisibiility("on")
     console.log("[INFO]: Generating fractal with method: Reductive.")
 }
 
 function generate_formulaic() {
+    setOutputCanvasVisibiility("on")
     console.log("[INFO]: Generating fractal with method: Formulaic.")
 }
